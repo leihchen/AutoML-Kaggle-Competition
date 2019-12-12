99f333b5414ea5ef7dbba75688b62b077ed3ef0a
陈雷昊 <chenleihao@taou.com>
2019-12-12 15:23:44 -0600

    submitted 19_1 97.091%

--- testing_backups/pytorch_net1.py
+++
@@ -1,9 +0,0 @@
-from torchvision.models import resnet50
-from thop import profile
-import torch
-model = resnet50()
-print(model)
-input = torch.randn(1, 3, 224, 224)
-flops, params = profile(model, inputs=(input, ))
-
-
